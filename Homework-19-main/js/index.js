daaf4b3df6d7c6c7ab40b073e235927224ee1df4
mackenzie-aitchison <99145286+mackenzie-aitchison@users.noreply.github.com>
2022-07-25 15:57:34 -0500

    Add files via upload

---
+++ Homework-19-main/js/index.js
@@ -0,0 +1,11 @@
+console.log("yourindex");
+
+
+
+
+
+  $(".button").hover(function(){
+    $(this).css("background-color", "blue");
+    }, function(){
+    $(this).css("background-color", "#373738");
+  });
